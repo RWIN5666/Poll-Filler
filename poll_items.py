c70b3c63561646e90a753c26da8e38215c714ba9
Erwin ANDRE <erwin.andre.work@gmail.com>
2019-05-26 00:30:01 +0200

    Now filling second page.

--- poll_items.py
+++ poll_items.py
@@ -8,4 +8,19 @@ class Poll_Item:
 
 
 def create_item_list(items_file_path):
-    
+    items_file=open(items_file_path, "r")
+    if items_file.mode == 'r':
+        items_list = []
+        items_lines = items_file.readlines()
+        for elt_line in items_lines:
+            print(elt_line)   
+            #first word is item's name
+            #second word between parenthesis is the link
+            #between brackets the number of times the item has been chosen in previous polls
+            item_name = elt_line[:elt_line.find("(")]
+            item_link = elt_line[elt_line.find("(")+1:elt_line.find(")")]
+            print(f"item link {item_link} and item name {item_name}")
+            new_item = Poll_Item(item_name, item_link)
+            items_list.append(new_item)
+        return items_list
+            
--- run_poll_filler.py
+++ run_poll_filler.py
@@ -2,7 +2,7 @@
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-import infos
+import infos, poll_items
 
 def fill_infos(browser):
     name_form = browser.find_element_by_id('yourname')
@@ -28,22 +28,41 @@ def set_poll_settings(browser):
     if(infos.receive_mail_for_each_comment == True):
         browser.find_element_by_id('receiveNewComments').click()
         
-
-
-
-def fill_first_poll_page():
+def fill_first_poll_page(browser):
     # opts = Options()
     # opts.set_headless()
     # assert opts.headless  # Operating in headless mode
     #browser = Firefox(options=opts)
-    browser = Firefox()
-    browser.get('https://framadate.org/create_poll.php?type=autre')
     fill_infos(browser)
     set_poll_settings(browser)
     submit_button_infos_page = browser.find_element_by_xpath('/html/body/div[3]/main/div[1]/div/form/p/button')
     submit_button_infos_page.click()    
 
+def fill_second_poll_page(browser):
+    items_list = []
+    items_list = poll_items.create_item_list("Poll Filler/items.txt")
+    for elt in items_list:
+        print(f"out item name {elt.name} & item link {elt.image_link}")
+    # TODO: check list size if it is superior to 5
+    more_items = len(items_list) - 5
+    if more_items > 0:
+        # TODO: if superior click on + button on the page
+        add_choice_button = browser.find_element_by_xpath('//*[@id="add-a-choice"]')
+        remaining_items = more_items
+        while remaining_items > 0:
+            add_choice_button.click()
+            remaining_items = remaining_items - 1
+    for x in range(0,len(items_list)):
+        id_string = "choice" + str(x)
+        choice_form = browser.find_element_by_id(id_string)
+        choice_form.send_keys(items_list[x].name)
+        # TODO: fill the form with items names
 
-
+    browser.find_element_by_xpath('/html/body/div[1]/main/form/div[1]/div/div[8]/button').click()
+    browser.find_element_by_xpath('/html/body/div[3]/main/form/div/div/p/button[2]').click()
+        
 if __name__ == "__main__":
-    fill_first_poll_page()
+    browser = Firefox()
+    browser.get('https://framadate.org/create_poll.php?type=autre')
+    fill_first_poll_page(browser)
+    fill_second_poll_page(browser)
