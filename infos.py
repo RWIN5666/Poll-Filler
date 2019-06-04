75286f812404692cc8d86f23845e0bb636eadf6a
Yoann Diquélou <yoann.diquelou@gmail.com>
2019-06-05 00:12:40 +0200

    Configure poll

--- infos.py
+++ infos.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python3
+import json, sys
 
-name = "Your Name"
-email = "yourmail@somewhere.com"
-poll_title = "Your title"
-Description = ""
-people_can_only_modify_their_vote = True
-receive_mail_for_each_vote = True
-receive_mail_for_each_comment = True
-use_image_instead_of_text = False
-language = "EN"
+class Infos:
+    def missing_key(self, key):
+        print("key {} is missing in configuration file".format(key))
+        sys.exit(-1)
+
+    def __init__(self, file):
+        with open(file) as json_configuration:
+            data = json.load(json_configuration)
+            if not 'name' in data:
+                self.missing_key('name')
+            self.name = data['name']
+            if not 'email' in data:
+                self.missing_key('email')
+            self.email = data['email']
+            if not 'poll_title' in data:
+                self.missing_key('poll_title')
+            self.poll_title = data['poll_title']
+            if not 'description' in data:
+                self.missing_key('description')
+            self.description = data['description']
+            if not 'people_can_only_modify_their_vote' in data:
+                self.missing_key('people_can_only_modify_their_vote')
+            self.people_can_only_modify_their_vote = data['people_can_only_modify_their_vote']
+            if not 'receive_mail_for_each_vote' in data:
+                self.missing_key('receive_mail_for_each_vote')
+            self.receive_mail_for_each_vote = data['receive_mail_for_each_vote']
+            if not 'receive_mail_for_each_comment' in data:
+                self.missing_key('receive_mail_for_each_comment')
+            self.receive_mail_for_each_comment = data['receive_mail_for_each_comment']
+            if not 'use_image_instead_of_text' in data:
+                self.missing_key('use_image_instead_of_text')
+            self.use_image_instead_of_text = data['use_image_instead_of_text']
+            if not 'language' in data:
+                self.missing_key('language')
+            self.language = data['language']
+            if not 'item_file' in data:
+                self.missing_key('item_file')
+            self.item_file = data['item_file']
+
+    
--- run_poll_filler.py
+++ run_poll_filler.py
@@ -2,54 +2,56 @@
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-import infos, poll_items
+from infos import Infos
+import poll_items
+import getopt, sys
 
 
-def fill_infos(browser):
+def fill_infos(browser, configuration):
     name_form = browser.find_element_by_id("yourname")
-    name_form.send_keys(infos.name)
+    name_form.send_keys(configuration.name)
 
     email_form = browser.find_element_by_id("email")
-    email_form.send_keys(infos.email)
+    email_form.send_keys(configuration.email)
 
     poll_title_form = browser.find_element_by_id("poll_title")
-    poll_title_form.send_keys(infos.poll_title)
+    poll_title_form.send_keys(configuration.poll_title)
 
     description_form = browser.find_element_by_id("poll_comments")
-    description_form.send_keys(infos.Description)
+    description_form.send_keys(configuration.description)
 
 
-def set_poll_settings(browser):
+def set_poll_settings(browser, configuration):
     facultative_settings_button = browser.find_element_by_xpath(
         "/html/body/div[3]/main/div[1]/div/form/div[7]/a"
     )
     facultative_settings_button.click()
-    if infos.people_can_only_modify_their_vote == True:
+    if configuration.people_can_only_modify_their_vote == True:
         browser.find_element_by_xpath(
             "/html/body/div[3]/main/div[1]/div/form/div[9]/div[4]/div/div/label[2]"
         ).click()
-    if infos.receive_mail_for_each_vote == True:
+    if configuration.receive_mail_for_each_vote == True:
         browser.find_element_by_id("receiveNewVotes").click()
-    if infos.receive_mail_for_each_comment == True:
+    if configuration.receive_mail_for_each_comment == True:
         browser.find_element_by_id("receiveNewComments").click()
 
 
-def fill_first_poll_page(browser):
+def fill_first_poll_page(browser, configuration):
     # opts = Options()
     # opts.set_headless()
     # assert opts.headless  # Operating in headless mode
     # browser = Firefox(options=opts)
-    fill_infos(browser)
-    set_poll_settings(browser)
+    fill_infos(browser, configuration)
+    set_poll_settings(browser, configuration)
     submit_button_infos_page = browser.find_element_by_xpath(
         "/html/body/div[3]/main/div[1]/div/form/p/button"
     )
     submit_button_infos_page.click()
 
 
-def fill_second_poll_page(browser):
+def fill_second_poll_page(browser, configuration):
     items_list = []
-    items_list = poll_items.create_item_list("items.txt")
+    items_list = poll_items.create_item_list(configuration.item_file)
     for elt in items_list:
         print(f"out item name {elt.name} & item link {elt.image_link}")
     # TODO: check list size if it is superior to 5
@@ -76,11 +78,30 @@ def complete_poll_creation(browser):
     CREATE_POLL_BUTTON_XPATH = "//button[contains(text(),'Create the poll')]"
     browser.find_element_by_xpath(CREATE_POLL_BUTTON_XPATH).click()
 
+def usage():
+    print("python run_poll_filler.py [--config=config_file.json]")
 
 if __name__ == "__main__":
+    # Use config.json as default config file
+    config_file = 'config.json'
+    # Get argments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "config="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+    # Parse arguments for configuration file
+    for o, a in opts:
+        if o in ('-c', '--config'):
+            config_file=a
+        elif o in ('-h', '--help'):
+            usage()
+            sys.exit()
+    configuration = Infos(config_file)
     browser = Firefox()
     browser.get("https://framadate.org/create_poll.php?type=autre")
-    fill_first_poll_page(browser)
-    fill_second_poll_page(browser)
+    fill_first_poll_page(browser, configuration)
+    fill_second_poll_page(browser, configuration)
     CREATE_POLL_BUTTON_XPATH = "//button[@name='confirmation']"
     browser.find_element_by_xpath(CREATE_POLL_BUTTON_XPATH).click()
