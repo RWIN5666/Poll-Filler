d121accc205efab78e371d91300cb5d0445e88cd
Yoann Diquélou <yoann.diquelou@gmail.com>
2019-06-06 22:31:03 +0200

    Use regex to parse items to be more robust in case of misformatting

--- poll_items.py
+++ poll_items.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 
 class Poll_Item:
     def __init__(self, name, image_link):
@@ -8,7 +9,12 @@ class Poll_Item:
 
 
 def create_item_list(items_file_path):
+    """
+    Parse a file to retrive items on it.
+    Items must be formated as follow: name(link)
+    """
     items_file = open(items_file_path, "r")
+    pattern = re.compile(r'(.*)\((.*)\)')
     if items_file.mode == "r":
         items_list = []
         items_lines = items_file.readlines()
@@ -17,9 +23,12 @@ def create_item_list(items_file_path):
             # first word is item's name
             # second word between parenthesis is the link
             # between brackets the number of times the item has been chosen in previous polls
-            item_name = elt_line[: elt_line.find("(")]
-            item_link = elt_line[elt_line.find("(") + 1 : elt_line.find(")")]
-            print(f"item link {item_link} and item name {item_name}")
-            new_item = Poll_Item(item_name, item_link)
-            items_list.append(new_item)
+            m = pattern.match(elt_line)
+            if None != m:
+                item_name = m.group(1)
+                item_link = m.group(2)
+                print(f"item link {item_link} and item name {item_name}")
+                new_item = Poll_Item(item_name, item_link)
+                items_list.append(new_item)
+            
         return items_list
